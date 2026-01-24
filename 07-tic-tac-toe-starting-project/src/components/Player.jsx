68510d6285406cb2f62f5a52842ac6a2a7606770
horatius83 <horatius83@gmail.com>
2026-01-24 09:17:56 -0600

    tic-tac-toe

---
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export default function Player({initialName, symbol, isActive}) {
+    const [isEditing, setIsEditing] = useState(false);
+    const [playerName, setPlayerName] = useState(initialName);
+    function handleEditClick() {
+        setIsEditing((editing) => !editing);
+    }
+    function handleChange(event) {
+        console.log(event);
+        setPlayerName(event.target.value);
+    }
+    return (
+        <li className={isActive ? 'active' : undefined}>
+          <span className="player">
+            ({!isEditing ? <span className="player-name">{playerName}</span> : <input onChange={handleChange} value={playerName} type="text" required></input>})
+            <span className="player-symbol">{symbol}</span>
+          </span>
+          <button onClick={handleEditClick}>{!isEditing ? 'Edit' : 'Save'}</button>
+        </li>
+    );
+}
