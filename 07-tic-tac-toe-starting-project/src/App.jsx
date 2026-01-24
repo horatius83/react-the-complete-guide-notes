fc8c010ca4c90de4923e10d5fcddc4ddce139cb3
M Monster <horatius83@gmail.com>
2026-01-24 11:14:21 -0600

    Final tic-tac-toe

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -4,8 +4,14 @@ import Player from "./components/Player"
 import GameBoard from "./components/GameBoard"
 import Log from "./components/Log"
 import { WINNING_COMBINATIONS } from "./winning-combinations"
+import GameOver from "./components/GameOver"
 
-const initialGameBoard = [
+const PLAYERS = {
+  'X': 'Player 1', 
+  'O': 'Player 2'
+};
+
+const INITIAL_GAME_BOARD = [
     [null, null, null],
     [null, null, null],
     [null, null, null]
@@ -18,26 +24,36 @@ function deriveActivePlayer(gameTurns) {
   return 'X';
 }
 
-function App() {
-  const [gameTurns, setGameTurns] = useState([]);
-
-  const activePlayer = deriveActivePlayer(gameTurns);
-  let gameBoard = initialGameBoard;
-  for (const turn of gameTurns) {
-      const { square, player } = turn;
-      const { row, col } = square;
-      gameBoard[row][col] = player;
-  }
-
-  let winner = null;
+function deriveWinner(gameBoard, players) {
   for (const combination of WINNING_COMBINATIONS) {
     const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
     const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
     const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
     if (firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol) {
-      winner = firstSquareSymbol;
+      return players[firstSquareSymbol];
     }
   }
+}
+
+function deriveGameboard(gameTurns) {
+  let gameBoard = [...INITIAL_GAME_BOARD.map(array => [...array])];
+  for (const turn of gameTurns) {
+      const { square, player } = turn;
+      const { row, col } = square;
+      gameBoard[row][col] = player;
+  }
+  return gameBoard;
+}
+
+function App() {
+  const [gameTurns, setGameTurns] = useState([]);
+  const [players, setPlayers] = useState(PLAYERS);
+
+  const activePlayer = deriveActivePlayer(gameTurns);
+  const gameBoard = deriveGameboard(gameTurns);
+  const winner = deriveWinner(gameBoard, players);
+
+  const isDraw = gameTurns.length === 9 && !winner;
 
   function handleSelectSquare(rowIndex, colIndex) {
     setGameTurns(prevTurns => {
@@ -51,13 +67,39 @@ function App() {
 
   }
 
+  function handleRematch() {
+    setGameTurns([]);
+  }
+
+  function handlePlayerNameChange(symbol, newName) {
+    setPlayers((prevPlayers) => {
+      return {
+        ...prevPlayers,
+        [symbol]: newName
+      };
+    });
+  }
+
   return <main>
     <div id="game-container">
       <ol id="players" className="highlight-player">
-        <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
-        <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
+        <Player 
+          initialName={PLAYERS.X}
+          symbol="X" 
+          isActive={activePlayer === 'X'}
+          onPlayerNameChange={handlePlayerNameChange}
+        />
+        <Player 
+          initialName={PLAYERS.O}
+          symbol="O" 
+          isActive={activePlayer === 'O'}
+          onPlayerNameChange={handlePlayerNameChange}
+        />
       </ol>
-      {winner && <p>You won, {winner}!</p>}
+      {(winner || isDraw) && <GameOver 
+        winner={winner} 
+        handleRematch={handleRematch}/>
+      }
       <GameBoard 
         onSelectSquare={handleSelectSquare} 
         board={gameBoard}
---
+++ 07-tic-tac-toe-starting-project/src/components/GameOver.jsx
@@ -0,0 +1,10 @@
+export default function GameOver({winner, handleRematch}) {
+    return <div id="game-over">
+        <h2>Game Over!</h2>
+        {winner 
+            ? <p>{winner} won!</p>
+            : <p>It&apos;s a draw!</p>
+        }
+        <p><button onClick={handleRematch}>Rematch!</button></p>
+    </div>
+}
