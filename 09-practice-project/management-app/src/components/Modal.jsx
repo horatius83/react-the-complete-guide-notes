5fe8b027cc469e21c05bfb1377da37b04cb85524
M Monster <horatius83@gmail.com>
2026-02-05 20:46:19 -0600

    09 styling

--- 09-practice-project/management-app/src/components/Modal.jsx
+++ 09-practice-project/management-app/src/components/Modal.jsx
@@ -1,5 +1,6 @@
 import { createPortal } from "react-dom"
 import { useImperativeHandle, useRef } from "react";
+import Button from './Button.jsx';
 
 export default function Modal({children, ref, buttonCaption}) {
     const dialog = useRef();
@@ -11,10 +12,10 @@ export default function Modal({children, ref, buttonCaption}) {
         };
     });
     return createPortal(
-        <dialog ref={dialog}>
+        <dialog ref={dialog} className="backdrop:bg-stone-900/90 p-4 rounded-md shadow-md">
             {children}
-            <form method="dialog">
-                <button>{buttonCaption}</button>
+            <form method="dialog" className="mt-4 text-right">
+                <Button>{buttonCaption}</Button>
             </form>
         </dialog>,
         document.getElementById('modal-root')
