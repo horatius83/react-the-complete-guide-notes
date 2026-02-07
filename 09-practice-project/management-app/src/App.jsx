0f9c3fe9191a7191edf0ea6f7f1b9508cac52644
horatius83 <horatius83@gmail.com>
2026-02-07 08:59:50 -0600

    Section 9: finished

--- 09-practice-project/management-app/src/App.jsx
+++ 09-practice-project/management-app/src/App.jsx
@@ -2,20 +2,37 @@ import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectSidebar from "./components/ProjectSidebar";
 import { useState } from 'react';
+import SelectedProject from "./components/SelectedProject";
 
 function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
-    projects: []
+    projects: [],
+    tasks: []
   });
 
-  function handleStartAddProject() {
-      setProjectsState(prevState => {
+  function handleAddTask(text) {
+     setProjectsState(prevState => {
+      const taskId = Math.random();
+      const newTask = {
+        text: text,
+        projectId: prevState.selectedProjectId,
+        id: taskId
+      };
+      return {
+        ...prevState,
+        tasks: [...prevState.tasks, newTask]
+      }
+    })   
+  }
+
+  function handleDeleteTask(id) {
+     setProjectsState(prevState => {
         return {
           ...prevState,
-          selectedProjectId: null
+          tasks: prevState.tasks.filter((task) => task.id != id)
         }
-      });
+      });   
   }
 
   function handleAddProject(projectData) {
@@ -27,22 +44,71 @@ function App() {
       };
       return {
         ...prevState,
-        selectedProjectId: undefined,
+        selectedProjectId: projectId,
         projects: [...prevState.projects, newProject]
       }
     })
   }
 
-  let content;
+  function handleStartAddProject() {
+      setProjectsState(prevState => {
+        return {
+          ...prevState,
+          selectedProjectId: null
+        }
+      });
+  }
+
+  function handleCancelAddProject() {
+    setProjectsState(prevState => {
+        return {
+          ...prevState,
+          selectedProjectId: undefined
+        }
+      });
+  }
+
+  function handleSelectProject(id) {
+    setProjectsState(prevState => {
+        return {
+          ...prevState,
+          selectedProjectId: id
+        }
+      });
+  }
+
+  function handleDeleteProject() {
+    setProjectsState(prevState => {
+        return {
+          ...prevState,
+          selectedProjectId: undefined,
+          projects: prevState.projects.filter((project) => project.id !== prevState.selectedProjectId)
+        }
+      });
+  }
+
+  const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId);
+  let content = <SelectedProject 
+    tasks={projectsState.tasks}
+    project={selectedProject} 
+    onDelete={handleDeleteProject} 
+    onAddTask={handleAddTask}
+    onDeleteTask={handleDeleteTask}
+    selectedProjectId={projectsState.selectedProjectId}
+  />;
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject onAdd={handleAddProject} />
+    content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
   }
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects} />
+      <ProjectSidebar 
+        onStartAddProject={handleStartAddProject} 
+        projects={projectsState.projects} 
+        onSelectProject={handleSelectProject}
+      />
       {content}
     </main>
   );
--- 09-practice-project/management-app/src/components/NewProject.jsx
+++ 09-practice-project/management-app/src/components/NewProject.jsx
@@ -2,7 +2,7 @@ import Input from "./Input";
 import Modal from "./Modal";
 import { useRef } from 'react';
 
-export default function NewProject({ onAdd }) {
+export default function NewProject({ onAdd, onCancel}) {
     const modal = useRef();
     const title = useRef()
     const description = useRef();
@@ -32,7 +32,7 @@ export default function NewProject({ onAdd }) {
         </Modal>
         <div className="w-[35rem] mt-16">
             <menu className="flex items-center justify-end gap-4 my-4">
-                <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
+                <li><button onClick={onCancel} className="text-stone-800 hover:text-stone-950">Cancel</button></li>
                 <li><button onClick={handleSave} className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
             </menu>
             <div>
