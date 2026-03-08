7b536bfd8ef39fac68bf2027623b09be92139d0f
horatius83 <horatius83@gmail.com>
2026-03-08 09:22:48 -0500

    redux basics section 20

---
+++ section-20/01-starting-project/src/App.js
@@ -0,0 +1,18 @@
+import Counter from './components/Counter';
+import Header from './components/Header';
+import Auth from './components/Auth';
+import { useSelector } from 'react-redux';
+
+function App() {
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+
+  return (
+    <>
+      {isAuthenticated && <Header /> }
+      {!isAuthenticated && <Auth /> }
+      <Counter />
+    </>
+  );
+}
+
+export default App;
---
+++ section-20/01-starting-project/src/components/Header.js
@@ -0,0 +1,28 @@
+import classes from './Header.module.css';
+import { useDispatch } from 'react-redux';
+import { authActions } from '../store/auth';
+
+const Header = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <header className={classes.header}>
+      <h1>Redux Auth</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href='/'>My Products</a>
+          </li>
+          <li>
+            <a href='/'>My Sales</a>
+          </li>
+          <li>
+            <button onClick={() => dispatch(authActions.logout())}>Logout</button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
